86925df0d0865e44a4691f0ba5fe53d8f6d2bf7e
yiyunzhu <zhu.yiy@husky.neu.edu>
2018-05-27 15:06:13 -0700

    modified user admin

--- src/main/java/com/example/webdev2018/sevices/UserService.java
+++ src/main/java/com/example/webdev2018/sevices/UserService.java
@@ -1,5 +1,71 @@
 package com.example.webdev2018.sevices;
 
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.webdev2018.models.User;
+import com.example.webdev2018.repositories.UserRepository;
+
+
+
+@RestController
 public class UserService {
+	@Autowired
+	UserRepository repository;
+	
+	// findAll
+	@GetMapping("/api/user")
+	public List<User> findAllUsers() {
+		return (List<User>)repository.findAll();
+	}
+	
+	// createNewUser
+	@PostMapping("/api/user")
+	public User createUser(@RequestBody User user) {
+		return repository.save(user);
+	}
+	
+	// deleteById
+	@DeleteMapping("/api/user/{userId}")
+	public void deleteUser(@PathVariable("userId") int id) {
+		repository.deleteById(id);
+	}
+	
+	// findById
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(@PathVariable("userId") int id) {
+		Optional<User> data = repository.findById(id);
+		if(data.isPresent()) {
+			return data.get();
+		}
+		return null;
+	}
+	
+	// update
+	@PutMapping("/api/user/{userId}")
+	public User updateUser(@PathVariable("userId") int id, @RequestBody User newUser) {
+		Optional<User> data = repository.findById(id);
+		if(data.isPresent()) {
+			User user = data.get();
+			user.setPassword(newUser.getPassword());
+			user.setFirstName(newUser.getFirstName());
+			user.setLastName(newUser.getLastName());
+			user.setRole(newUser.getRole());
+			repository.save(user);
+			return user;
+		}
+		return null;
+	}
+
 
 }
--- src/main/webapp/jquery/services/user.service.client.js
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -4,11 +4,7 @@ function UserServiceClient() {
 	this.findUserById = findUserById;
 	this.deleteUser = deleteUser;
 	this.updateUser = updateUser;
-	this.register = register;
-	this.login = login;
-	this.getProfile = getProfile;
-	this.updateProfile = updateProfile;
-	this.logout = logout;
+
    	this.url = 'http://localhost:8080/api/user';
    	this.reg = 'http://localhost:8080/api/register';
    	this.logi = 'http://localhost:8080/api/login';
@@ -16,73 +12,6 @@ function UserServiceClient() {
    	this.logo = 'http://localhost:8080/api/logout';
    	var self = this;
 
-   	function getProfile() {
-   		return fetch(self.pro, {
-   			credentials: 'same-origin'
-   			})
-   			.then(res => res.json())
-			.catch(function (err){
-				return null;
-			})
-			.then(function (res){
-				return res;
-			});
-   	} 
-
-   	
-   	function updateProfile(user) {
-   		return fetch(self.pro, {
-   			credentials: 'same-origin',
-   			method: 'put',
-   			body: JSON.stringify(user),
-			headers: {
-				'content-type': 'application/json'
-			}
-   		})
-   		.then(res => res.json())
-		.catch(function (err){
-			return null;
-		})
-		.then(function (res){
-			return res;
-		});
-   	}
-
-   	function logout() {
-   		return fetch(self.logo, {
-   			method: 'put',
-   			credentials: 'same-origin'
-		});
-   	}
-
-   	function register(user) {
-		return fetch(self.reg, {
-			method: 'post',
-			body: JSON.stringify(user),
-			headers: {
-				'content-type': 'application/json'
-			},
-			credentials: 'same-origin'
-		})
-		.then(res => res.json())
-		.catch(function (err){
-			return null;
-		})
-		.then(function (res){
-			return res;
-		});
-	}
-
-	function login(user) {
-		return fetch(self.logi, {
-			credentials: 'same-origin',
-			method: 'put',
-			body: JSON.stringify(user),
-			headers: {
-				'content-type': 'application/json'
-			}			
-		});
-	}
 
    function updateUser(userId, user) {
 		return fetch(self.url + '/' + userId, {
