faf1763c198e9e32b2d9de2a05624f066de4cc1f
yiyunzhu <zhu.yiy@husky.neu.edu>
2018-05-27 14:31:33 -0700

    add user model client

---
+++ src/main/webapp/jquery/models/user.model.client.js
@@ -0,0 +1,9 @@
+function User(username, password, firstName, lastName, role) {
+	this.username = username;
+	this.password = password;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.role = role;
+	var self = this;
+
+}
